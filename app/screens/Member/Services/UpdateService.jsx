4ea98976a6832a1693ff29b1cbee97513a44944f
ranawaqartufail <ranawaqarroyal@gmail.com>
2020-06-08 18:12:28 +0500

    service file updated

--- app/screens/Member/Services/UpdateService.jsx
+++ app/screens/Member/Services/UpdateService.jsx
@@ -1,5 +1,14 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+  TopNavigation,
+  Divider,
+  Layout,
+  Text,
+  Button,
+  Modal,
+  Card,
+} from '@ui-kitten/components';
 
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
@@ -9,7 +18,11 @@ import {
   PhoneNumberValidator,
 } from '../../../components/validators/Validators';
 import WhiteSpace from '../../../components/Space/WhiteSpace';
-import { ServiceCategories } from '../../../util/servicesCategories';
+import ActivityIndicatorOverlay from '../../../components/ActivityIndicator/ActivityIndicatorOverlay';
+import { ServiceCategories } from './../../../util/servicesCategories';
+import styles from './../../../navigator/styles';
+
+const categories = ServiceCategories;
 
 export default function UpdateMemberService() {
   return (
@@ -64,8 +77,69 @@ export default function UpdateMemberService() {
           >
             Update Service
           </Button>
+          <ActivityIndicatorOverlay
+            visible={deleting}
+            text={'Deleting service...'}
+          />
+          <ActivityIndicatorOverlay
+            visible={deleting}
+            text={'Deleting service...'}
+          />
+          <Modal
+            visible={modelVisible}
+            backdropStyle={styles.backdrop}
+            onBackdropPress={() => setModelVisible(false)}
+          >
+            <Card disabled={true}>
+              <Text style={{ textAlign: 'center' }}>
+                Do you confirm deleting this service?
+              </Text>
+              <WhiteSpace size={2} />
+              <View
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                }}
+              >
+                <Button
+                  onPress={() => setModelVisible(false)}
+                  status='basic'
+                  style={{ width: '40%' }}
+                >
+                  No
+                </Button>
+                <Button
+                  onPress={onDeleteService}
+                  style={{ width: '40%' }}
+                  status='danger'
+                >
+                  Yes
+                </Button>
+              </View>
+            </Card>
+          </Modal>
         </Layout>
       </KeyboardAwareScrollView>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    paddingBottom: 70,
+  },
+  thumbnail: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+  },
+  thumbnailView: {
+    textAlign: 'center',
+  },
+  backdrop: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+});
